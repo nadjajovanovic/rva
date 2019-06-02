dbfd867f7b8c5a9cede12202d3d830c0482f65d7
nadjajovanovic <j.nadja97@gmail.com>
2019-06-02 18:10:47 +0200

    cross origin

--- BackendRVA/src/main/java/rva/ctrls/KlijentRestController.java
+++ BackendRVA/src/main/java/rva/ctrls/KlijentRestController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import rva.jpa.Klijent;
 import rva.reps.KlijentRepository;
 
 @Api(tags = {"Klijent CRUD operacije"})
+@CrossOrigin
 @RestController
 public class KlijentRestController {
 	
--- BackendRVA/src/main/java/rva/ctrls/TipRacunaRestController.java
+++ BackendRVA/src/main/java/rva/ctrls/TipRacunaRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import rva.jpa.TipRacuna;
 import rva.reps.TipRacunaRepository;
 
 @Api(tags = {"Tip racuna CRUD operacije"})
+@CrossOrigin
 @RestController
 public class TipRacunaRestController {
 	
